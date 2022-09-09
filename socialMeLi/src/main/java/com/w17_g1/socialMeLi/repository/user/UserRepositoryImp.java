5022355a7cda31e6877e8f26639aa03bea4d316d
nprantl <nicolas.prantl@mercadolibre.com>
2022-09-09 15:42:00 -0300

    Se agrega loadDataBase

--- socialMeLi/src/main/java/com/w17_g1/socialMeLi/repository/user/UserRepositoryImp.java
+++ socialMeLi/src/main/java/com/w17_g1/socialMeLi/repository/user/UserRepositoryImp.java
@@ -1,7 +1,39 @@
 package com.w17_g1.socialMeLi.repository.user;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.w17_g1.socialMeLi.model.User;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 @Repository
 public class UserRepositoryImp implements IUserRepository {
+    private final List<User> users;
+
+    public UserRepositoryImp() {
+        this.users = loadDataBase();
+    }
+
+    private List<User> loadDataBase() {
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:users.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<User>> typeRef = new TypeReference<>() {};
+        List<User> users = null;
+        try {
+            users = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
 }
