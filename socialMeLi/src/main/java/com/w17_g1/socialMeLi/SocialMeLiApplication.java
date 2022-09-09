435c52d6aa792553decee9338dc5815d5e6a74a4
Tomas Lodola <tomas.lodola@mercadolibre.com>
2022-09-09 12:19:41 -0300

    Initial project

---
+++ socialMeLi/src/main/java/com/w17_g1/socialMeLi/SocialMeLiApplication.java
@@ -0,0 +1,13 @@
+package com.w17_g1.socialMeLi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMeLiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMeLiApplication.class, args);
+	}
+
+}
---
+++ socialMeLi/src/test/java/com/w17_g1/socialMeLi/SocialMeLiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w17_g1.socialMeLi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMeLiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
