d8fcecb514380386a578fce9ec9576303133d6bf
Tomas Lodola <tomas.lodola@mercadolibre.com>
2022-09-12 12:33:35 -0300

    devolviendo la lista de publicaciones ordenadas y mapeandolas al DTO

--- socialMeLi/src/main/java/com/w17_g1/socialMeLi/services/PublicationService.java
+++ socialMeLi/src/main/java/com/w17_g1/socialMeLi/services/PublicationService.java
@@ -1,14 +1,27 @@
 package com.w17_g1.socialMeLi.services;
 
 
+import com.w17_g1.socialMeLi.dto.output.PublicationDTO;
 import com.w17_g1.socialMeLi.repository.publication.IPublicationRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PublicationService {
 
     @Autowired
     IPublicationRepository publicationRepository;
+
+    private ModelMapper mapper = new ModelMapper();
+    public List<PublicationDTO> getPublicationListSorted(Integer userId,String order){
+        var publications =  publicationRepository.sortPublicationList(userId,order);
+
+        return publications.stream()
+                .map(p->mapper.map(p,PublicationDTO.class))
+                .collect(Collectors.toList());
+    }
 }
