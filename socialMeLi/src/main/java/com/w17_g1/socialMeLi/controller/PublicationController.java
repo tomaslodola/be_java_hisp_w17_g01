afa4b5e8fb68dbb924d4793ccc3cdd7bd8ed0ed6
Tomas Lodola <tomas.lodola@mercadolibre.com>
2022-09-12 10:51:26 -0300

    creando el controlador que devuelve la lista ordenada solo si recibe el parametro order

--- socialMeLi/src/main/java/com/w17_g1/socialMeLi/controller/PublicationController.java
+++ socialMeLi/src/main/java/com/w17_g1/socialMeLi/controller/PublicationController.java
@@ -1,11 +1,25 @@
 package com.w17_g1.socialMeLi.controller;
 
+import com.w17_g1.socialMeLi.services.PublicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PublicationController {
+
+    @Autowired
+    PublicationService publicationSrv;
+
+    @GetMapping("/products/followed/{userId}/list")
+    public ResponseEntity<?> getProducts(@PathVariable Integer userId, @RequestParam(value = "order",required = false) String order){
+        if(order!=null)
+            return new ResponseEntity<>(publicationSrv)
+
+    }
 
 }
