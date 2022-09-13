190319707f4298eb4b99b70db803223f52b924d4
Tomas Lodola <tomas.lodola@mercadolibre.com>
2022-09-13 13:11:57 -0300

    declarando nuevos metodos reutilizables

--- socialMeLi/src/main/java/com/w17_g1/socialMeLi/repository/publication/IPublicationRepository.java
+++ socialMeLi/src/main/java/com/w17_g1/socialMeLi/repository/publication/IPublicationRepository.java
@@ -1,5 +1,6 @@
 package com.w17_g1.socialMeLi.repository.publication;
 
+import com.w17_g1.socialMeLi.dto.input.PublicationDTO;
 import com.w17_g1.socialMeLi.model.Publication;
 import java.util.List;
 import java.time.LocalDate;
@@ -8,4 +9,7 @@ import java.util.Optional;
 public interface IPublicationRepository {
   List<Publication> getPublicationsFromUser(Integer userId, LocalDate searchAfterDate);
   Optional<Publication> createPublication(Publication publication);
+  Boolean createPromoPublication(Publication publication);
+  Boolean create(Publication publication);
+
 }
