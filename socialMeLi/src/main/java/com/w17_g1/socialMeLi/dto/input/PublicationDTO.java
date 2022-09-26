3409b5420deeb9e01e688aac6bdb198aa5e1c1cf
nprantl <nicolas.prantl@mercadolibre.com>
2022-09-26 16:14:19 -0300

    Validaciones parte 1

--- socialMeLi/src/main/java/com/w17_g1/socialMeLi/dto/input/PublicationDTO.java
+++ socialMeLi/src/main/java/com/w17_g1/socialMeLi/dto/input/PublicationDTO.java
@@ -8,6 +8,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
@@ -15,10 +18,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class PublicationDTO {
+    @Min(message = "El id debe ser mayor a cero",value = 1L)
+    @NotNull(message = "El  id no puede estar vacío.")
     private Integer user_id;
+
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @NotNull(message = "La fecha no puede estar vacía.")
     private LocalDate date;
+
+    @Valid
     private ProductDTO product;
     private Integer category;
     private Double price;
