a251f3d6fa9b5d37282b592339865cc9eabeeb65
nprantl <nicolas.prantl@mercadolibre.com>
2022-09-26 10:56:47 -0300

    Pull de main

--- socialMeLi/src/main/java/com/w17_g1/socialMeLi/exceptions/ElementNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.w17_g1.socialMeLi.exceptions;
-
-
-public class ElementNotFoundException extends RuntimeException{
-    public ElementNotFoundException() {}
-    public ElementNotFoundException(String message) {
-        super(message);
-    }
-}
